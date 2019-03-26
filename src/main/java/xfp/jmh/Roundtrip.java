a84ad1161b35810c773e931779a2d668d7f2612c
John McDonald <palisades.lakes@gmail.com>
2019-03-26 10:28:55 -0700

    double -> Rational -> double benchmark

---
+++ src/main/java/xfp/jmh/Roundtrip.java
@@ -0,0 +1,54 @@
+package xfp.jmh;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.rng.UniformRandomProvider;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import xfp.java.numbers.Doubles;
+import xfp.java.numbers.Rational;
+import xfp.java.prng.Generator;
+import xfp.java.prng.PRNG;
+
+// java -ea --illegal-access=warn -jar target/benchmarks.jar Roundtrip
+
+/** Benchmark roundtrip double -> rational -> double.
+ * <pre>
+ * java -ea -jar target\benchmarks.jar Sum
+ * </pre>
+ * @author palisades dot lakes at gmail dot com
+ * @version 2019-03-26
+ */
+@SuppressWarnings("unchecked")
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class Roundtrip {
+
+  //--------------------------------------------------------------
+
+  private static final int TRYS = 1*1024;
+ 
+  @SuppressWarnings("static-method")
+  @Benchmark
+  public final boolean roundtrip () { 
+    final UniformRandomProvider urp = 
+      PRNG.well44497b("seeds/Well44497b-2019-01-05.txt");
+    final Generator g = Doubles.finiteGenerator(urp);
+    boolean identical = true;
+    for (int i=0;i<TRYS;i++) {
+      final double x0 = g.nextDouble();
+      final Rational q = Rational.valueOf(x0);
+      final double x1 = q.doubleValue();
+      identical = identical && (x1 == x0); }
+    assert identical;
+    return identical; }
+
+  //--------------------------------------------------------------
+}
+//--------------------------------------------------------------
