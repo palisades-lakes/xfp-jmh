91862728ff95859029c5f2ad372ebb6fae6e140a
John Alan McDonald <palisades.lakes@gmail.com>
2019-07-29 14:30:13 -0700

    minor

--- src/main/java/xfp/jmh/All.java
+++ src/main/java/xfp/jmh/All.java
@@ -4,21 +4,21 @@ package xfp.jmh;
  * java -cp target\benchmarks.jar xfp.jmh.All
  * </pre>
  * @author palisades dot lakes at gmail dot com
- * @version 2019-07-25
+ * @version 2019-07-29
  */
 
 public final class All {
   public static final void main (final String[] args)  {
-    Defaults.run("TotalSum");
-    Defaults.run("TotalL1Norm");
+    //Defaults.run("TotalSum");
+    //Defaults.run("TotalL1Norm");
     Defaults.run("TotalL2Norm");
     Defaults.run("TotalDot");
-    Defaults.run("TotalL2Distance");
-    Defaults.run("TotalL1Distance");
-    Defaults.run("PartialSums");
-    Defaults.run("PartialL1s");
+    //Defaults.run("TotalL2Distance");
+    //Defaults.run("TotalL1Distance");
+    //Defaults.run("PartialSums");
+    //Defaults.run("PartialL1s");
     Defaults.run("PartialL2s");
     Defaults.run("PartialDots");
-    Defaults.run("PartialL2Distances");
-    Defaults.run("PartialL1Distances");
+    //Defaults.run("PartialL2Distances");
+    //Defaults.run("PartialL1Distances");
   } }
