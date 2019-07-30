de65333aa80f48b0f8c951fce78b0b5a135b7a50
John McDonald <palisades.lakes@gmail.com>
2019-07-29 22:07:09 -0700

    revert All

--- src/main/java/xfp/jmh/All.java
+++ src/main/java/xfp/jmh/All.java
@@ -9,16 +9,16 @@ package xfp.jmh;
 
 public final class All {
   public static final void main (final String[] args)  {
-    //Defaults.run("TotalSum");
-    //Defaults.run("TotalL1Norm");
-    Defaults.run("TotalL2Norm");
+    Defaults.run("TotalSum");
     Defaults.run("TotalDot");
-    //Defaults.run("TotalL2Distance");
-    //Defaults.run("TotalL1Distance");
-    //Defaults.run("PartialSums");
-    //Defaults.run("PartialL1s");
-    Defaults.run("PartialL2s");
+    Defaults.run("TotalL1Norm");
+    Defaults.run("TotalL2Norm");
+    Defaults.run("TotalL2Distance");
+    Defaults.run("TotalL1Distance");
+    Defaults.run("PartialSums");
     Defaults.run("PartialDots");
-    //Defaults.run("PartialL2Distances");
-    //Defaults.run("PartialL1Distances");
+    Defaults.run("PartialL1s");
+    Defaults.run("PartialL2s");
+    Defaults.run("PartialL2Distances");
+    Defaults.run("PartialL1Distances");
   } }
