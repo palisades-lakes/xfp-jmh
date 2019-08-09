2040a152a3fe6b2c8b58970e790388cbd8a41ba9
John McDonald <palisades.lakes@gmail.com>
2019-08-09 10:51:49 -0700

    minor

--- src/main/java/xfp/jmh/Base.java
+++ src/main/java/xfp/jmh/Base.java
@@ -26,7 +26,7 @@ import xfp.java.test.Common;
  * java -cp target\benchmarks.jar xfp.jmh.Base
  * </pre>
  * @author palisades dot lakes at gmail dot com
- * @version 2019-08-04
+ * @version 2019-08-08
  */
 
 @SuppressWarnings("unchecked")
