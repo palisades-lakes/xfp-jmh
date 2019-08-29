897ed729ea3554ac0d6aa3528b9dbef958815560
John McDonald <palisades.lakes@gmail.com>
2019-08-28 21:57:25 -0700

    do all generators

--- src/main/java/xfp/jmh/Base.java
+++ src/main/java/xfp/jmh/Base.java
@@ -22,7 +22,7 @@ import xfp.java.test.Common;
  * java -cp target\benchmarks.jar xfp.jmh.Base
  * </pre>
  * @author palisades dot lakes at gmail dot com
- * @version 2019-08-26
+ * @version 2019-08-28
  */
 
 @SuppressWarnings("unchecked")
