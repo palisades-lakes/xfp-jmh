45bfb16f34f88d8807f192a4f1cf06eef7b992dc
John Alan McDonald <palisades.lakes@gmail.com>
2019-10-08 14:51:48 -0700

    general monomial polynomials

--- src/main/java/xfp/jmh/MonomialBench.java
+++ src/main/java/xfp/jmh/MonomialBench.java
@@ -10,50 +10,64 @@ import org.openjdk.jmh.annotations.TearDown;
 
 import xfp.java.accumulators.Accumulator;
 import xfp.java.accumulators.EFloatAccumulator;
-import xfp.java.polynomial.EFloatCubic;
+import xfp.java.polynomial.MonomialEFloat;
 import xfp.java.polynomial.Polynomial;
 import xfp.java.prng.Generator;
 import xfp.java.prng.Generators;
 import xfp.java.test.Common;
 
-/** Benchmark axpy implementations.
+/** Benchmark polynomial implementations.
  *
  *<pre>
- * j xfp.jmh.CubicBench
+ * j xfp.jmh.MonomialBench
  * </pre>
  * 
  * @author palisades dot lakes at gmail dot com
- * @version 2019-10-07
+ * @version 2019-10-08
  */
 
 @SuppressWarnings("unchecked")
 @State(Scope.Thread)
-public class CubicBench {
+public class MonomialBench {
 
   //--------------------------------------------------------------
 
   //@Param({"exponential",})
   //@Param({"finite",})
   //@Param({"gaussian",})
-  //@Param({"laplace",})
+  @Param({"laplace",})
   //@Param({"uniform",})
-  @Param({"exponential","finite","gaussian","laplace","uniform",})
-  String generator;
-  Generator gen;
+  //@Param({"exponential","finite","gaussian","laplace","uniform",})
+  String agenerator;
+  Generator agen;
+
+  @Param({
+    //"1",
+    //"2",
+    "3",
+  })
+  int degree;
+  
+  double[] a;
+
+  @Param({
+    //"xfp.java.polynomial.MonomialDouble",
+    "xfp.java.polynomial.MonomialDoubleBF",
+    "xfp.java.polynomial.MonomialBigFloat",
+    //"xfp.java.polynomial.MonomialDoubleRF",
+    //"xfp.java.polynomial.MonomialRationalFloat",
+    })
+  String cName;
+  Polynomial c;
+
+  // estimated value(s)
+  double[] p;
+
+  //--------------------------------------------------------------
 
   Polynomial e;
   // exact value(s)
   double[] truth;
-
-  @Param({
-    "xfp.java.polynomial.RationalFloatCubic",
-    "xfp.java.polynomial.BigFloatCubic",
-    //"xfp.jmh.polynomial.BigFloat0Cubic",
-    //"xfp.java.polynomial.DoubleCubic",
-    //"xfp.java.polynomial.EFloatCubic",
-  })
-  String cName;
-  Polynomial c;
 
   //--------------------------------------------------------------
 
@@ -64,18 +78,20 @@ public class CubicBench {
     //"2097153",
     //"1048575",
     //"524289",
-    "131071",
+    //"131071",
+    "32767",
   })
   int dim;
 
-  double a0;
-  double a1;
-  double a2;
-  double a3;
+  //@Param({"exponential",})
+  //@Param({"finite",})
+  @Param({"gaussian",})
+  //@Param({"laplace",})
+  //@Param({"uniform",})
+  //@Param({"exponential","finite","gaussian","laplace","uniform",})
+  String xgenerator;
+  Generator xgen;
   double[] x;
-
-  // estimated value(s)
-  double[] p;
 
   //--------------------------------------------------------------
   /** This is what is timed. */
@@ -91,19 +107,17 @@ public class CubicBench {
    */
   @Setup(Level.Trial)
   public final void trialSetup () {
-    gen = Generators.make(generator,dim); }
+    agen = Generators.make(xgenerator,degree+1);
+    xgen = Generators.make(xgenerator,dim); }
 
   @Setup(Level.Invocation)
   public final void invocationSetup () {
-    a0 = gen.nextDouble();
-    a1 = gen.nextDouble();
-    a2 = gen.nextDouble();
-    a3 = gen.nextDouble();
-    x = (double[]) gen.next();
-    e = EFloatCubic.make(a0,a1,a2,a3);
+    a = (double[]) agen.next();
+    x = (double[]) xgen.next();
+    e = MonomialEFloat.make(a);
     assert e.isExact();
     truth = operation(e,x);
-    c = Common.makeCubic(cName,a0,a1,a2,a3); }
+    c = Common.makeMonomial(cName,a); }
 
   private final Accumulator acc = EFloatAccumulator.make();
 
@@ -120,7 +134,7 @@ public class CubicBench {
   //--------------------------------------------------------------
 
   public static final void main (final String[] args)  {
-    Defaults.run("CubicBench"); } 
+    Defaults.run("MonomialBench"); } 
 
   //--------------------------------------------------------------
 }
